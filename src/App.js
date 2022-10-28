4216547d018a8da894aece928fd40afff7caeb9c
qoudri4re <qoudri4re@gmail.com>
2022-10-28 12:28:07 +0100

    --

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import slack_image from "./slack.png";
+import github_image from "./Icon.png";
+import profile_image from "./profile-image.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <div className="innerContent">
+        <div className="image-container">
+          <img src={profile_image} alt="" id="profile_img" />
+          <h4>Abdulqodir Muritadoh</h4>
+        </div>
+        <div className="buttons">
+          <div className="button">
+            <button>Twitter</button>
+          </div>
+          <div className="button">
+            <button>
+              <a href="https://training.zuri.team/" id="btn_zuri">
+                Zuri Team
+              </a>
+            </button>
+          </div>
+          <div className="button">
+            <button>
+              <a href="http://books.zuri.team" id="books">
+                Zuri Books
+              </a>
+            </button>
+          </div>
+          <div className="button">
+            <button>
+              <a
+                href="https://books.zuri.team/python-for-beginners?ref_id=abdulqodir"
+                id="book__python"
+              >
+                Makes sense, buy
+              </a>
+            </button>
+          </div>
+          <div className="button">
+            <button>
+              <a href="https://background.zuri.team">Just buy it</a>
+            </button>
+          </div>
+          <div className="button">
+            <button>
+              <a href="https://books.zuri.team/design-rules" id="book__design">
+                Design Books
+              </a>
+            </button>
+          </div>
+        </div>
+        <div className="social">
+          <img src={slack_image} alt="" />
+          <img src={github_image} alt="" />
+        </div>
+      </div>
+      <div className="footer"></div>
     </div>
   );
 }
